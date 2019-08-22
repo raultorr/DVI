dabda8394f6b5a81866feddec91a3a6d25cf78af
Carlos León <cleon@ucm.es>
2019-08-22 18:02:49 +0200

    quitamos un archivo y simplificamos

--- game.js
+++ game.js
@@ -1,22 +1,13 @@
-import Main from './main.js'
-
-export default class Game extends Phaser.Game {
+export default class Main extends Phaser.Scene {
   constructor() {
-    var config = {
-      type: Phaser.AUTO,
-      width: 1400,
-      height: 800,
-      pixelArt: true,
-      input: {
-        gamepad: true
-      },
-      scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-      },
-      scene: [Main],
-      physics: { default: 'arcade', arcade: { gravity: { y: 400 }, debug: false } }
-    };
-    super(config);
+    super({ key: 'main' });
+  }
+  preload() {  
+  }
+
+  create() {
+  }
+
+  update(time, delta) {    
   }
 }

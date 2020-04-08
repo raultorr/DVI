332d13a92857b0fd46fe2ce2ac32a8a04958976c
alvant01 <alvant01@ucm.es>
2020-04-08 21:26:13 +0200

    Test Menu and TestMap

--- src/game.js
+++ src/game.js
@@ -5,11 +5,15 @@ export default class Game extends Phaser.Scene {
 
 	}
 	preload() {  
-		this.load.spritesheet('run', '../DVI/assets/sprites/RunAnimation/run.png',{ frameWidth: 16, frameHeight: 32 });
-  		this.load.image("tiles", "../DVI/Maps/Assets.png");
-  		this.load.tilemapTiledJSON("map", "../DVI/Maps/PJson.json");
+		this.load.spritesheet('run', 'assets/sprites/runAnimation/run.png',{ frameWidth: 16, frameHeight: 32 });
+		this.load.spritesheet('jump', 'assets/sprites/runAnimation/jump.png',{ frameWidth: 16, frameHeight: 32 });
+  		this.load.image("tiles", "Maps/Assets.png");
+  		this.load.tilemapTiledJSON("map", "Maps/PJson.json");
+
 	}
 	create() {
+
+		var scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 		/*
 		const backGround = map.createStaticLayer("Capa de patrones 2", tileset , 0 , 0);
 		 */
@@ -26,14 +30,13 @@ export default class Game extends Phaser.Scene {
 		worldLayer.setCollisionByProperty({ collides: true });
 
 	    this.player = this.physics.add.sprite(200, 450, 'run');
-
 	    //this.physics.add.collider(this.player, this.worldLayer);
 	    this.physics.add.collider(this.player, worldLayer);
 
 
 		this.player.setBounce(0.2);
 		this.player.setCollideWorldBounds(true);
-
+		
 		this.player.body.setGravityY(50);
 		this.anims.create({
 		    key: 'left',
@@ -42,46 +45,86 @@ export default class Game extends Phaser.Scene {
 		    repeat: -1
 		});
 
-	this.anims.create({
-	    key: 'turn',
-	    frames: [ { key: 'run', frame: 5 } ],
-	    frameRate: 1
-	});
+		this.anims.create({
+		    key: 'turn',
+		    frames: [ { key: 'run', frame: 5 } ],
+		    frameRate: 1
+		});
 
-	this.anims.create({
-	    key: 'right',
-	    frames: this.anims.generateFrameNumbers('run', { start: 0, end: 4 }),
-	    frameRate: 10,
-	    repeat: -1
-	});
+		this.anims.create({
+		    key: 'right',
+		    frames: this.anims.generateFrameNumbers('run', { start: 0, end: 4 }),
+		    frameRate: 10,
+		    repeat: -1
+		});
+		this.anims.create({
+		key: 'rightJump',
+		frames: this.anims.generateFrameNumbers('jump', {start: 0, end: 4}),
+		frameRate: 5,
+		repeat: -1
+		});
+		this.anims.create({
+			key: 'leftJump',
+			frames: this.anims.generateFrameNumbers('jump', {start: 6, end: 10}),
+			frameRate: 5,
+			repeat: -1
+		});
 
-
-	this.cursors = this.input.keyboard.createCursorKeys();
-}
-
-update(time, delta) {   
-	if (this.cursors.left.isDown)
-	{
-	    this.player.setVelocityX(-160);
-
-	    this.player.anims.play('left', true);
-	}
-	else if (this.cursors.right.isDown)
-	{
-	    this.player.setVelocityX(160);
-
-	    this.player.anims.play('right', true);
-	}
-	else
-	{
-	    this.player.setVelocityX(0);
-
-	   this.player.anims.play('turn');
+		this.cursors = this.input.keyboard.createCursorKeys();
 	}
 
-	if (this.cursors.up.isDown && this.player.body.touching.down)
-	{
-	    this.player.setVelocityY(-330);
+	update(time, delta) {
+
+		if(this.cursors.up.isDown && this.cursors.right.isDown)
+		{
+			if(this.player.body.onFloor())
+				this.player.setVelocityY(-330);
+			this.player.setVelocityX(160);
+
+		    this.player.anims.play('rightJump', true);
+		}
+		else if(this.cursors.up.isDown && this.cursors.left.isDown)
+		{
+			if(this.player.body.onFloor())
+				this.player.setVelocityY(-330);
+			this.player.setVelocityX(-160);
+
+		    this.player.anims.play('leftJump', true);
+		}
+
+
+		else if(this.cursors.left.isDown)
+		{
+		    this.player.setVelocityX(-160);
+
+		    this.player.anims.play('left', true);
+		}
+		else if (this.cursors.right.isDown)
+		{
+		    this.player.setVelocityX(160);
+
+		    this.player.anims.play('right', true);
+		}
+		else if (this.cursors.up.isDown)
+		{
+			if(this.player.body.onFloor())
+		    	this.player.setVelocityY(-330);
+			
+			/*else if(this.cursors.up.isDown || !this.player.body.onFloor())
+			{
+				this.player.setVelocityY(-330);
+				this.player.anims.play('leftJump', true);
+			}*/
+
+		}
+		
+		else
+		{
+		    this.player.setVelocityX(0);
+
+		   this.player.anims.play('turn');
+		}
+
+		
 	}
-}
 }
--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,82 @@
-export default class Menu extends Phaser.Scene {
+export default class Game extends Phaser.Scene {
 
-	constructor() {
-   		super({ key: 'main' });
-  	}
-  	preload() { 
-  		game.load.image('backGround', 'assets/backGround.jpg');
-  	}
+  constructor(configGame) {
+      super({ key: 'menu' });
 
-  	create() {
-  		this.add.image(400,300, 'backGround');
-  	}
+  }
+  preload() {  
+      this.load.image("backGround", "assets/backGround.png");
+      this.load.image("tv", "assets/menuTV.png");
+      this.load.image("tvButton", "assets/prueba.png");
+      this.load.image("play", "assets/play_button.png");
+      this.load.image("options", "assets/options_button.png");
+      this.load.image("cursor", "assets/cursor.png");
+      this.load.image("bg", "assets/static.png");
+  }
+  create() {
+  this.add.image(this.scale.width / 2, this.scale.height / 2 ,'bg').setScale(3);
+    this.add.image(this.scale.width / 2, this.scale.height / 2 ,'tv');
+   var button = this.add.sprite(1240, 800,'tvButton');
 
-  	update(time, delta) {    
-  	}
+  let playButton = this.add.sprite(300, 200,'play');
+  let optionsButton = this.add.sprite(300, 300,'options');
 
+
+  let hoverSprite= Phaser.GameObjects.Sprite = this.add.sprite(100, 100, 'cursor');
+    hoverSprite.setScale(2);
+    hoverSprite.setVisible(false);
+
+
+
+
+
+    playButton.setInteractive();
+
+    playButton.on("pointerover", () => {
+        hoverSprite.setVisible(true);
+        //hoverSprite.play("walk");
+        hoverSprite.x = playButton.x - playButton.width;
+        hoverSprite.y = playButton.y;
+        button.angle -= 6;
+
+    })
+
+    playButton.on("pointerout", () => {
+        hoverSprite.setVisible(false);
+         button.angle += 6;
+    })
+  playButton.on("pointerdown", () => {
+       this.scene.start('main');
+       //new Phaser.Game(config);
+    })
+
+   optionsButton.setInteractive();
+
+    optionsButton.on("pointerover", () => {
+        hoverSprite.setVisible(true);
+        //hoverSprite.play("walk");
+        hoverSprite.x = optionsButton.x - optionsButton.width;
+        hoverSprite.y = optionsButton.y;
+        button.angle -= 12;
+
+    })
+
+    optionsButton.on("pointerout", () => {
+        hoverSprite.setVisible(false);
+         button.angle += 12;
+    })
+   /* optionsButton.on("pointerup", () => {
+        this.scene.start(CST.SCENES.PLAY);
+    })*/
+
+  this.cursors = this.input.keyboard.createCursorKeys();
+
+  if (this.cursors.down.isDown)
+  {
+    button.angle -= 6;
+  }
+  }
+
+  //update(time, delta) {   
+ 
 }

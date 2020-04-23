d50179176d0647a86199df6a9f2e094da24013ae
alvant01 <alvant01@ucm.es>
2020-04-23 03:16:08 +0200

    replaced deleted files

--- src/game.js
+++ src/game.js
@@ -1,5 +1,3 @@
-import Player from './player.js';
-import Load   from './load.js';
 export default class Game extends Phaser.Scene {
 
 	constructor(configGame) {
@@ -7,23 +5,16 @@ export default class Game extends Phaser.Scene {
 
 	}
 	preload() {  
-		this.load.spritesheet('laserOn', 'assets/sprites/laser/laser-turn-on.png',{ frameWidth: 5, frameHeight: 50 });
-		this.load.spritesheet('laserOff', 'assets/sprites/laser/laser-turn-off.png',{ frameWidth: 5, frameHeight: 50 });
+		this.load.spritesheet('run', 'assets/sprites/RunAnimation/run.png',{ frameWidth: 16, frameHeight: 32 });
+		this.load.spritesheet('jump', 'assets/sprites/RunAnimation/jump.png',{ frameWidth: 16, frameHeight: 32 });
+		this.load.spritesheet('crouch', 'assets/sprites/RunAnimation/crouch.png',{ frameWidth: 16, frameHeight: 25 });
   		this.load.image("tiles", "Maps/Assets.png");
   		this.load.tilemapTiledJSON("map", "Maps/PJson.json");
-  		this.load.spritesheet('run', 'assets/sprites/runAnimation/run.png',{ frameWidth: 16, frameHeight: 32 });
-        this.load.spritesheet('jump', 'assets/sprites/runAnimation/jump.png',{ frameWidth: 16, frameHeight: 32 });
-        this.load.spritesheet('crouch', 'assets/sprites/runAnimation/crouch.png',{ frameWidth: 16, frameHeight: 25 });
 
 	}
 	create() {
 
-
-		//let load = new Load(this, 0 ,0)
-
-		//load.animsPlayer();
-
-
+		var scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 		/*
 		const backGround = map.createStaticLayer("Capa de patrones 2", tileset , 0 , 0);
 		 */
@@ -32,34 +23,146 @@ export default class Game extends Phaser.Scene {
 		const tileset = map.addTilesetImage("Assets2", "tiles");
 
 
-		const backGround = map.createStaticLayer("Capa de patrones 2", tileset , 0 , 0).setPipeline('Light2D');
+		const backGround = map.createStaticLayer("Capa de patrones 2", tileset , 0 , 0);
+		const backGround2 = map.createStaticLayer("Fondo 2", tileset , 0 , 0);
 		const worldLayer = map.createStaticLayer("Capa de patrones 1", tileset , 0 , 0);
-		
-
+	
 
 		worldLayer.setCollisionByProperty({ collides: true });
 
-
-
-	    this.player = new Player(this, 200, 450);
+	    this.player = this.physics.add.sprite(200, 450, 'run');
 	    //this.physics.add.collider(this.player, this.worldLayer);
 	    this.physics.add.collider(this.player, worldLayer);
-		//var lights = this.lights.addLight(500,200,200);
-		//this.lights.enable().setAmbientColor(0x555555);
 
-		//this.player.setCollideWorldBounds(true);
+
+		this.player.setBounce(0.2);
+		this.player.setCollideWorldBounds(true);
 		
-		//this.player.body.setGravityY(50);
+		this.player.body.setGravityY(50);
+		this.anims.create({
+		    key: 'left',
+		    frames: this.anims.generateFrameNumbers('run', { start: 6, end: 10 }),
+		    frameRate: 10,
+		    repeat: -1
+		});
 
+		this.anims.create({
+		    key: 'turn',
+		    frames: [ { key: 'run', frame: 5 } ],
+		    frameRate: 1
+		});
 
+		this.anims.create({
+		    key: 'right',
+		    frames: this.anims.generateFrameNumbers('run', { start: 0, end: 4 }),
+		    frameRate: 10,
+		    repeat: -1
+		});
+		
+		this.anims.create({
+		key: 'rightJump',
+		frames: this.anims.generateFrameNumbers('jump', {start: 0, end: 4}),
+		frameRate: 5,
+		repeat: -1
+		});
+		this.anims.create({
+			key: 'leftJump',
+			frames: this.anims.generateFrameNumbers('jump', {start: 6, end: 10}),
+			frameRate: 5,
+			repeat: -1
+		});
 
-		//laser = this.physics.add.sprite(500,450, 'laserOn');
+		this.anims.create({
+			key: 'leftCrouch',
+			frames: this.anims.generateFrameNumbers('crouch', {start: 5, end: 9}),
+			frameRate: 5,
+			repeat: -1
+		});
+
+		this.anims.create({
+			key: 'rightCrouch',
+			frames: this.anims.generateFrameNumbers('crouch', {start: 0, end: 4}),
+			frameRate: 5,
+			repeat: -1
+		});
+
 
 		this.cursors = this.input.keyboard.createCursorKeys();
 	}
 
 	update(time, delta) {
+		this.player.setSize(16, 32);
+		this.player.setOffset(0, 0);
+		if(this.cursors.up.isDown && this.cursors.right.isDown)
+		{
+			if(this.player.body.onFloor())
+				this.player.setVelocityY(-330);
+			this.player.setVelocityX(160);
 
-		this.player.update();
+		    this.player.anims.play('rightJump', true);
+		}
+		else if(this.cursors.up.isDown && this.cursors.left.isDown)
+		{
+			if(this.player.body.onFloor())
+				this.player.setVelocityY(-330);
+			this.player.setVelocityX(-160);
+
+		    this.player.anims.play('leftJump', true);
+		}
+
+		if(this.cursors.down.isDown && this.cursors.right.isDown)
+		
+		{
+			this.player.setSize(16, 20);
+			this.player.setOffset(0,5 );
+			if(this.player.body.onFloor())
+				this.player.setVelocityX(80);
+
+		    this.player.anims.play('rightCrouch', true);
+		}else if(this.cursors.down.isDown && this.cursors.left.isDown)
+		
+		{
+			this.player.setSize(16, 20);
+			this.player.setOffset(0, 5);
+			if(this.player.body.onFloor())
+				this.player.setVelocityX(-80);
+
+		    this.player.anims.play('leftCrouch', true);
+		}
+
+
+		else if(this.cursors.left.isDown)
+		{
+		    this.player.setVelocityX(-160);
+
+		    this.player.anims.play('left', true);
+		}
+		else if (this.cursors.right.isDown)
+		{
+		    this.player.setVelocityX(160);
+
+		    this.player.anims.play('right', true);
+		}
+		else if (this.cursors.up.isDown)
+		{
+			if(this.player.body.onFloor())
+		    	this.player.setVelocityY(-330);
+			
+			/*else if(this.cursors.up.isDown || !this.player.body.onFloor())
+			{
+				this.player.setVelocityY(-330);
+				this.player.anims.play('leftJump', true);
+			}*/
+
+		}
+		
+		else
+		{
+		    this.player.setVelocityX(0);
+
+		   this.player.anims.play('turn');
+		}
+
+		
 	}
 }
